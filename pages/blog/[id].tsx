6c32f5d5e3942257498182068ea77a3def7d7eeb
cawauchi6204 <cawauchi6204@gmail.com>
2022-08-12 10:18:50 +0900

    I: シンタックスハイライトの実装

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,8 +1,11 @@
 import { client } from "../../libs/client"
 import CategoryButton from "../../components/categoryButton"
 import Image from "next/image"
+import * as cheerio from 'cheerio';
+import hljs from 'highlight.js'
+import 'highlight.js/styles/night-owl.css';
 
-export default function BlogId({ content }: any) {
+export default function BlogId({ content,highlightedBody }: any) {
   return (
     <main>
       <div className="max-w-3xl bg-black mt-16">
@@ -19,9 +22,9 @@ export default function BlogId({ content }: any) {
           />
         </div>
         <div
-          className="tracking-wider leading-loose"
+          className="tracking-wider leading-loose mt-8"
           dangerouslySetInnerHTML={{
-            __html: `${content.content}`,
+            __html: highlightedBody,
           }}
         />
       </div>
@@ -35,9 +38,17 @@ export const getStaticProps = async ({ params }: any) => {
   const id = params.id;
   const data = await client.get({ endpoint: "blogs", contentId: id });
 
+  const $ = cheerio.load(data.content);
+  $('pre code').each((_, elm) => {
+    const result = hljs.highlightAuto($(elm).text());
+    $(elm).html(result.value);
+    $(elm).addClass('hljs');
+  });
+
   return {
     props: {
       content: data,
+      highlightedBody: $.html()
     },
   };
 };
